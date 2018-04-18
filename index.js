76da0333ffd8f1c4f4f122241d39eeeb546d2421
Will <will.liu@nakedhub.com>
2018-04-18 18:01:10 +0800

    0.0.4

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+var downloadUrl = require('download')
+var gitclone = require('git-clone')
+var rm = require('rimraf').sync
 var yargs = require('yargs');
-var download = require('download-git-repo');
 var cmd = require('node-cmd');
 
 yargs.parse(process.argv.slice(2), (err, argv, output) => {
@@ -44,3 +46,103 @@ yargs.parse(process.argv.slice(2), (err, argv, output) => {
 		console.error('path [' + (path || name || '') + '] not found');
 	}
 });
+
+function download (repo, dest, opts, fn) {
+	if (typeof opts === 'function') {
+		fn = opts
+		opts = null
+	}
+	opts = opts || {}
+
+	repo = normalize(repo)
+	var url = getUrl(repo, opts)
+
+// console.log(url, repo);
+
+	if (opts.clone) {
+		gitclone(url, dest, { checkout: repo.checkout, shallow: repo.checkout === 'master' }, function (err) {
+			if (err === undefined) {
+				rm(dest + '/.git')
+				fn()
+			} else {
+				fn(err)
+			}
+		})
+	} else {
+		downloadUrl(url, dest, { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } }).then(data => {
+			fn()
+		}).catch(err => {
+			fn(err)
+		})
+	}
+}
+
+function normalize (repo) {
+	var regex = /^((github|gitlab|bitbucket):)?((.+):)?([^/]+)\/([^#]+)(#(.+))?$/
+	var match = regex.exec(repo)
+	var type = match[2] || 'custom'
+	var origin = match[4] || null
+	var owner = match[5]
+	var name = match[6]
+	var checkout = match[8] || 'master'
+
+	if (origin == null) {
+		if (type === 'github')
+			origin = 'github.com'
+		else if (type === 'gitlab')
+			origin = 'gitlab.com'
+		else if (type === 'bitbucket')
+			origin = 'bitbucket.org'
+	}
+
+	return {
+		type: type,
+		origin: origin,
+		owner: owner,
+		name: name,
+		checkout: checkout
+	}
+}
+
+function addProtocol (origin, clone) {
+	if (!/^(f|ht)tps?:\/\//i.test(origin)) {
+		if (clone)
+			origin = 'git@' + origin
+		else
+			origin = 'https://' + origin
+	}
+
+	return origin
+}
+
+function getUrl (repo, opts) {
+	var url
+
+	var origin = addProtocol(repo.origin, opts.clone)
+
+	if (/^git\@/i.test(origin))
+		origin = origin + ':'
+	else
+		origin = origin + '/'
+
+	// Build url
+	if (opts.clone) {
+		if (repo.type === 'custom')
+			url = repo.owner + '/' + repo.name;
+		else
+			url = origin + repo.owner + '/' + repo.name + '.git'
+	} else {
+		if (repo.type === 'github')
+			url = origin + repo.owner + '/' + repo.name + '/archive/' + repo.checkout + '.zip'
+		else if (repo.type === 'gitlab')
+			url = origin + repo.owner + '/' + repo.name + '/repository/archive.zip?ref=' + repo.checkout
+		else if (repo.type === 'bitbucket')
+			url = origin + repo.owner + '/' + repo.name + '/get/' + repo.checkout + '.zip'
+		else if (repo.type === 'custom')
+			url = repo.owner + '/' + repo.name;
+		else 
+			url = github(repo)
+	}
+
+	return url
+}
